ecc76ecf54af406d3b0cc1e55e96c77af3f07852
Luís da Silveira Carneiro Júnior <lscj.ldc@gmail.com>
2025-02-25 10:57:26 -0300

    Atualização história

--- js/app.js
+++ js/app.js
@@ -29,3 +29,66 @@ function btnAnoIF() {
     var element = document.getElementById("secaoDois");
     element.classList.remove("segundo-plano");
 }
+
+function btnEni() {
+    var element = document.getElementById("secaoTres");
+    element.classList.add("segundo-plano");
+}
+function btnEniF() {
+    var element = document.getElementById("secaoTres");
+    element.classList.remove("segundo-plano");
+}
+
+function btnV() {
+    var element = document.getElementById("secaoQuatro");
+    element.classList.add("segundo-plano");
+}
+function btnvF() {
+    var element = document.getElementById("secaoQuatro");
+    element.classList.remove("segundo-plano");
+}
+
+function btnJn() {
+    var element = document.getElementById("secaoCinco");
+    element.classList.add("segundo-plano");
+}
+function btnJnF() {
+    var element = document.getElementById("secaoCinco");
+    element.classList.remove("segundo-plano");
+}
+
+function btnAnoJn() {
+    var element = document.getElementById("secaoSeis");
+    element.classList.add("segundo-plano");
+}
+function btnAnoJnF() {
+    var element = document.getElementById("secaoSeis");
+    element.classList.remove("segundo-plano");
+}
+
+function btnAm() {
+    var element = document.getElementById("secaoSete");
+    element.classList.add("segundo-plano");
+}
+function btnAmF() {
+    var element = document.getElementById("secaoSete");
+    element.classList.remove("segundo-plano");
+}
+
+function btnAmAno() {
+    var element = document.getElementById("secaoOito");
+    element.classList.add("segundo-plano");
+}
+function btnAmAnoF() {
+    var element = document.getElementById("secaoOito");
+    element.classList.remove("segundo-plano");
+}
+
+function btnHm() {
+    var element = document.getElementById("secaoNove");
+    element.classList.add("segundo-plano");
+}
+function btnHmF() {
+    var element = document.getElementById("secaoNove");
+    element.classList.remove("segundo-plano");
+}
